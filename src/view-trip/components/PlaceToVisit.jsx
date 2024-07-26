8a79413fc369791a31290ec78cde86f2768c50e4
Aoffy <offfyclub@gmail.com>
2024-07-26 15:20:09 +0700

    update

--- src/view-trip/components/PlaceToVisit.jsx
+++ src/view-trip/components/PlaceToVisit.jsx
@@ -1,24 +1,23 @@
-
-import PlaceVisitCard from './PlaceVisitCard'
+import PlaceVisitCard from "./PlaceVisitCard";
 
 const PlaceToVisit = ({ trip }) => {
-    return (
-        <div>
-            <h2 className='text-2xl font-bold mt-5' >Place to Visit</h2>
-            <div>
-                {trip?.tripData?.itinerary?.map((item, index) => (
-                    <div key={index} className='mt-5'>
-                        <h3 className='text-lg font-bold'>{item?.day}</h3>
-                        <div className='grid md:grid-cols-2 gap-5'>
-                            {item?.plan?.map((plan, index) => (
-                                <PlaceVisitCard plan={plan} key={index}/>
-                            ))}
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mt-5">Place to Visit</h2>
+      <div>
+        {trip?.tripData?.itinerary?.map((item, index) => (
+          <div key={index} className="mt-5">
+            <h3 className="text-lg font-bold">{item?.day}</h3>
+            <div className="grid md:grid-cols-2 gap-5">
+              {item?.plan?.map((plan, index) => (
+                <PlaceVisitCard plan={plan} key={index} />
+              ))}
             </div>
-        </div>
-    )
-}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default PlaceToVisit
+export default PlaceToVisit;
